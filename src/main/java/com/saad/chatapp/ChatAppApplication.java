bf2dca4059daf9af0e2dedd88c83f91530604c4d
saad.qayyum <khawajasaadqayyum4110@gmail.com>
2024-08-27 08:52:00 +0300

    This is Spring Boot Application for Group Chat. Spring web-Sockets used for backend and java Script used for frontend

---
+++ src/main/java/com/saad/chatapp/ChatAppApplication.java
@@ -0,0 +1,14 @@
+package com.saad.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAppApplication.class, args);
+        System.out.println("chat App Using Spring Boot");
+    }
+
+}
